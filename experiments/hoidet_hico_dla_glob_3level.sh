ef85718b0b1a7217844d6cc725f8472c3b612ed2
YueLiao <liaoyue.ai@gmail.com>
2020-03-22 17:46:09 +0800

    Update hoidet_hico_dla_glob_3level.sh

--- experiments/hoidet_hico_dla_glob_3level.sh
+++ experiments/hoidet_hico_dla_glob_3level.sh
@@ -6,7 +6,7 @@ set -x
 cd src
 
 #train
-python -u  main.py hoidet --exp_id hoidet_hico_dla_3levelglob --arch dla3levelglob_34 --batch_size 112 --master_batch 7  --lr 4.5e-4 --gpus 0,1,2,3,4,5,6,7 --num_workers 16 --val_intervals 100000 --load_model ../models/ctdet_coco_dla_2x.pth --image_dir images/train2015 --dataset hico --root_path '/mnt/lustre/liaoyue/datasets'
+python -u  main.py hoidet --exp_id hoidet_hico_dla_3levelglob --arch dla3levelglob_34 --batch_size 104 --master_batch 6  --lr 4e-4 --gpus 0,1,2,3,4,5,6,7 --num_workers 16 --val_intervals 100000 --load_model ../models/ctdet_coco_dla_2x.pth --image_dir images/train2015 --dataset hico --root_path '/mnt/lustre/liaoyue/datasets'
 
 #test
 python -u test_hoi.py hoidet --exp_id hoidet_hico_dla_3levelglob --arch dla3levelglob_34 --gpus 0 --image_dir images/test2015 --dataset hico --test_with_eval
