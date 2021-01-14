7d2b6c15688e9959ef9548a99bbf7d2065738f57
Zhiyuan Chen <chenzhiyuan1@tetras.ai>
2021-01-14 22:24:46 +0800

    set loss smoking

--- src/lib/models/model.py
+++ src/lib/models/model.py
@@ -9,7 +9,7 @@ import torch.distributed as dist
 
 import models.networks as networks
 
-from .losses import FocalLoss, RegL1Loss, RegLoss, GIouLoss, generalized_box_iou
+from .losses import FocalLoss, RegL1Loss, RegLoss, GIouLoss, generalized_box_iou, sigmoid_focal_loss_jit
 from utils import clamped_sigmoid
 
 
@@ -101,12 +101,12 @@ class HoidetLoss(nn.Module):
         self.opt = opt
         self.states = [
             'loss', 'hm_loss', 'wh_loss', 'off_loss', 'hm_rel_loss',
-            'sub_offset_loss', 'obj_offset_loss'
+            'offset_loss'
         ]
 
     def forward(self, outputs, batch):
         opt = self.opt
-        hm_loss, wh_loss, off_loss, hm_rel_loss, sub_offset_loss, obj_offset_loss = 0, 0, 0, 0, 0, 0
+        hm_loss, wh_loss, off_loss, hm_rel_loss, offset_loss = 0, 0, 0, 0, 0, 0
 
         for s in range(opt.num_stacks):
             output = outputs[s]
@@ -200,7 +200,7 @@ class MinCostMatcher(nn.Module):
         batch_out_prob = output['hm_rel'].permute(0, 2, 3, 1).reshape(
             bs, h * w, k).sigmoid()  # [batch_size, num_queries, num_classes]
         batch_out_offset = torch.cat(
-            [output['obj_offset'], output['sub_offset']],
+            [output['sub_offset'], output['obj_offset']],
             1).permute(0, 2, 3, 1).reshape(bs, h * w, 4)  # [batch_size, num_queries, 4]
         batch_tgt_offset = batch['offset'].permute(0, 3, 4, 1, 2).reshape(bs, -1, h * w, 4)
 
@@ -251,72 +251,114 @@ class SetLoss(nn.Module):
         self.crit_reg = RegL1Loss() if opt.reg_loss == 'l1' else \
             RegLoss() if opt.reg_loss == 'sl1' else None
         self.crit_wh = self.crit_reg
+        self.focal_loss_alpha = 0.25
+        self.focal_loss_gamma = 2
         self.states = [
             'loss', 'hm_loss', 'wh_loss', 'off_loss', 'hm_rel_loss',
-            'sub_offset_loss', 'obj_offset_loss'
+            'offset_loss'
         ]
 
     def forward(self, outputs, batch):
         opt = self.opt
-        # hm_loss, wh_loss, off_loss, hm_rel_loss, sub_offset_loss, obj_offset_loss = 0, 0, 0, 0, 0, 0
-        losses = [0, 0, 0, 0, 0, 0]
+        # hm_loss, wh_loss, off_loss, hm_rel_loss, obj_offset_loss = 0, 0, 0, 0, 0
+        losses = [0, 0, 0, 0, 0]
 
         for s in range(opt.num_stacks):
             output = outputs[s]
 
             indices = self.matcher(output, batch)
-            losses_ = self.loss(output, batch)
+            losses_ = self.loss(output, batch, indices)
             losses = [i + x for i, x in zip(losses, losses_)]
 
-        hm_loss, wh_loss, off_loss, hm_rel_loss, sub_offset_loss, obj_offset_loss = losses
+        hm_loss, wh_loss, off_loss, hm_rel_loss, offset_loss = losses
 
         loss = opt.hm_weight * (hm_loss + hm_rel_loss) + opt.wh_weight * (
-            wh_loss + sub_offset_loss +
-            obj_offset_loss) + opt.off_weight * off_loss
+            wh_loss + offset_loss) + opt.off_weight * off_loss
         loss_states = {
             'loss': loss,
             'hm_loss': hm_loss,
             'wh_loss': wh_loss,
             'off_loss': off_loss,
             'hm_rel_loss': hm_rel_loss,
-            'sub_offset_loss': sub_offset_loss,
-            'obj_offset_loss': obj_offset_loss
+            'offset_loss': offset_loss
         }
         return loss, loss_states
 
-    def loss(self, output, batch):
-        hm_loss, hm_rel_loss = self.loss_cls(output, batch)
-        wh_loss, sub_offset_loss, obj_offset_loss, off_loss = self.loss_reg(
-            output, batch)
-        return hm_loss, wh_loss, off_loss, hm_rel_loss, sub_offset_loss, obj_offset_loss
+    def loss(self, output, batch, indices):
+        hm_loss, hm_rel_loss = self.loss_cls(output, batch, indices)
+        wh_loss, offset_loss, off_loss = self.loss_reg(output, batch, indices)
+        return hm_loss, wh_loss, off_loss, hm_rel_loss, offset_loss
 
-    def loss_cls(self, output, batch):
+    def _get_src_permutation_idx(self, indices):
+        # permute predictions following indices
+        batch_idx = torch.cat(
+            [torch.full_like(src, i) for i, (src, _) in enumerate(indices)])
+        src_idx = torch.cat([src for (src, _) in indices])
+        return batch_idx, src_idx
+
+    def loss_cls(self, output, batch, indices):
+        opt = self.opt
+        src_logits = output['hm_rel']
+        tgt_logits = batch['hm_rel']
+        bs, k, h, w = src_logits.shape
+        src_logits = src_logits.permute(0, 2, 3, 1).reshape(bs, h * w, k)
+
+        idx = self._get_src_permutation_idx(indices)
+        target_classes_o = torch.cat([t[J] for t, (_, J) in zip(tgt_logits, indices)])
+        target_classes = torch.full(src_logits.shape[:2],
+                                    117,
+                                    dtype=torch.int64,
+                                    device=src_logits.device)
+        target_classes[idx] = target_classes_o
+        src_logits = src_logits.flatten(0, 1)
+        target_classes = target_classes.flatten(0, 1)
+        pos_inds = torch.nonzero(target_classes != 117, as_tuple=True)[0]
+        labels = torch.zeros_like(src_logits)
+        labels[pos_inds, target_classes[pos_inds]] = 1
+
         output['hm'] = clamped_sigmoid(output['hm'])
-        output['hm_rel'] = clamped_sigmoid(output['hm_rel'])
+        output['hm_rel'] = sigmoid_focal_loss_jit(
+            src_logits,
+            labels,
+            alpha=self.focal_loss_alpha,
+            gamma=self.focal_loss_gamma,
+            reduction="sum",
+        )
         hm_loss = self.crit(output['hm'], batch['hm']) / self.opt.num_stacks
-        hm_rel_loss = self.crit(output['hm_rel'],
-                                batch['hm_rel']) / self.opt.num_stacks
+        hm_rel_loss = output['hm_rel'] / self.opt.num_stacks
+        #output['hm_rel'] = clamped_sigmoid(output['hm_rel'])
+        #hm_rel_loss = self.crit(output['hm_rel'],
+        #                        batch['hm_rel']) / self.opt.num_stacks
         return hm_loss, hm_rel_loss
 
-    def loss_reg(self, output, batch):
-        wh_loss, sub_offset_loss, obj_offset_loss, off_loss = 0, 0, 0, 0
+    def loss_reg(self, output, batch, indices):
+        wh_loss, offset_loss, off_loss = 0, 0, 0
+
+        tgt_offset = batch['offset']
+        bs, k, n, h, w = tgt_offset.shape
+
+        idx = self._get_src_permutation_idx(indices)
+        src_offset = torch.cat(
+            [output['sub_offset'], output['obj_offset']],
+            1).permute(0, 2, 3, 1).reshape(bs, h * w, 4)
+        src_offset = src_offset[idx]
+        dt=tgt_offset[0]
+        dj, di = indices[0]
+        ti = dt[di]
+        tgt_offset = torch.cat([t.flatten(-2, -1)[i, :, j] for t, (j, i) in zip(tgt_offset, indices)], dim=0)
+
+        output['offset_loss'] = F.l1_loss(src_offset, tgt_offset, reduction='none')
+        offset_loss = output['offset_loss'] / self.opt.num_stacks
+
         if self.opt.wh_weight > 0:
             wh_loss = self.crit_reg(output['wh'], batch['reg_mask'],
                                     batch['ind'],
                                     batch['wh']) / self.opt.num_stacks
-            sub_offset_loss = self.crit_reg(output['sub_offset'],
-                                            batch['offset_mask'],
-                                            batch['rel_ind'],
-                                            batch['sub_offset'])
-            obj_offset_loss = self.crit_reg(output['obj_offset'],
-                                            batch['offset_mask'],
-                                            batch['rel_ind'],
-                                            batch['obj_offset'])
         if self.opt.reg_offset and self.opt.off_weight > 0:
             off_loss = self.crit_reg(output['reg'], batch['reg_mask'],
                                      batch['ind'],
                                      batch['reg']) / self.opt.num_stacks
-        return wh_loss, sub_offset_loss, obj_offset_loss, off_loss
+        return wh_loss, offset_loss, off_loss
 
 
 class SetCriterion(nn.Module):
